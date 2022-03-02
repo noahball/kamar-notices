01202a0e62cb20267f4419c13fa6a9d4b39b9d19
Noah Ball <noah@noahball.com>
2022-03-03 08:49:15 +1300

    Start script

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ var bodyParser = require('body-parser')
 app.use(express.static('public'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 var notices = require('./routes/notices.js')
 
 app.get('/api/v1/notices', notices)
+
+app.listen(3000, () => {
+    console.log(`KAMAR Notices Service running on port 3000.`); // Run!
+});
